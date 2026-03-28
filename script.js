64db8472c290e304623b28c18bbad9473b921408
wendarov <chatgptakkplus@gmail.com>
2026-03-28 05:24:36 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const buttons = document.querySelectorAll('.buy-btn');
-  buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-      alert('Чтобы купить, напишите в Telegram — @wendarov');
-    });
-  });
-});
